a29de90c801fb3084d97eee4c7c519d7c4a178f4
sgaluzin <sgaluzin@gmail.com>
2025-06-22 18:19:30 +0300

    Reinstall symfony

--- public/index.php
+++ public/index.php
@@ -2,31 +2,8 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if ($_SERVER['APP_DEBUG'] ?? ($_ENV['APP_DEBUG'] ?? false)) {
-    umask(0000);
-    Symfony\Component\ErrorHandler\Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Symfony\Component\HttpFoundation\Request::setTrustedProxies(
-        explode(',', $trustedProxies),
-        Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
-        Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST |
-        Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO |
-        Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT |
-        Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PREFIX
-    );
-}
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Symfony\Component\HttpFoundation\Request::setTrustedHosts([
-        $trustedHosts
-    ]);
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool)($_SERVER['APP_DEBUG'] ?? ($_ENV['APP_DEBUG'] ?? false)));
-$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,37 +1,11 @@
 <?php
+
 namespace App;
 
-use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
-    protected function configureContainer(ContainerConfigurator $container): void
-    {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{services}.yaml');
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $routes->import('../config/{routes}.yaml');
-    }
-
-    public function registerBundles(): iterable
-    {
-        $contents = require dirname(__DIR__).'/config/bundles.php';
-        foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
-            }
-        }
-    }
-
-    public function registerContainerConfiguration(LoaderInterface $loader): void
-    {
-        $loader->load(dirname(__DIR__).'/config/packages/*.yaml', 'glob');
-        $loader->load(dirname(__DIR__).'/config/services.yaml');
-    }
+    use MicroKernelTrait;
 }
