2d79c3a43bfc47ea8e74d43b6149e6c1acca980a
sgaluzin <sgaluzin@gmail.com>
2025-06-22 19:02:40 +0300

    Add PHPUnit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testAddition(): void
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
+
